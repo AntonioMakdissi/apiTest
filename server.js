7c4104bb6687554a1cc2ea5810674838358aab1d
Antonio Al Makdissi <antoniomakdissi1@gmail.com>
2023-02-12 18:09:53 +0200

    added a new endpoint /data

--- server.js
+++ server.js
@@ -4,5 +4,14 @@ const PORT = process.env.PORT || 3030;
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
-//ok
+
+app.get('/data', function (req, res) {
+    var data= [
+        {id:17,name:'Omar'},
+        {id:58,name:'por'},
+        {id:23,name:'Omi'},
+    ]
+  res.send(data)
+})
+
 app.listen(PORT)
